f94df3a847d30bd0f173628e70eaa4ed6f2710bd
Gabriel Chimilevschi <gabriel@clevada.com>
2025-07-26 16:53:40 +0300

    Files update

--- src/SamplePackageServiceProvider.php
+++ src/SamplePackageServiceProvider.php
@@ -3,20 +3,17 @@
 namespace Pivlu\SamplePackage;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-use App\Models\Package;
 use Pivlu\SamplePackage\Console\Commands\SampleCommand;
 use Pivlu\SamplePackage\Http\Middleware\SampleMiddleware;
 
-
 class SamplePackageServiceProvider extends ServiceProvider
 {
-      
+
    public function boot()
    {
       // Register our package's middleware.
       $this->app['router']->aliasMiddleware('sample.middleware', SampleMiddleware::class);
-
+      
       // Publish configuration file.
       $this->publishes([
          __DIR__ . '/config/sample-package.php' => config_path('pivlu/sample-package.php'),
@@ -29,27 +26,34 @@ class SamplePackageServiceProvider extends ServiceProvider
       $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
       // Register views.
-      $this->loadViewsFrom(__DIR__ . '/resources/views', 'sample-package');
+      $this->loadViewsFrom(__DIR__ . '/resources/views', 'pivlu/sample-package');
 
       // Publish assets.
       $this->publishes([
          __DIR__ . '/resources/assets/' => public_path('packages/pivlu/sample-package'),
-      ], 'assets');
+      ], 'assets'); // <- The tag ("assets") is defined here will be used in `php artisan vendor:publish --tag=assets` command.
 
-      // Publish assets.
+      // Publish migrations.
       $this->publishes([
          __DIR__ . '/migrations' => database_path('migrations'),
-      ], 'migrations');  // <- The tag is defined here will be used in `php artisan vendor:publish --tag=HERE` command.
+      ], 'migrations');  // <- The tag ("migrations") is defined here will be used in `php artisan vendor:publish --tag=migrations` command.
 
-      // Register commands.
+      // Publish migrations.
+      $this->publishes([
+         __DIR__ . '/resources/views' => resource_path('views/vendor/pivlu/sample-package'),
+      ], 'views');  // <- The tag ("views") is defined here will be used in `php artisan vendor:publish --tag=views` command.
+
+      // Add package in database
+      \App\Models\Plugin::install(self::PLUGIN_VENDOR . '/' . self::PLUGIN_NAME);
+
       if ($this->app->runningInConsole()) {
+         // Register commands.
          $this->commands([
             SampleCommand::class,
          ]);
       }
-
-      Package::install('pivlu/sample-package');
    }
+
 
    public function register()
    {
---
+++ src/resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Pivlu SamplePackage</title>
+    <link rel="stylesheet" href="{{ asset('packages/sample-package/css/sample.css') }}">
+</head>
+
+<body>
+    <h1>{{ $greeting }}</h1>
+</body>
+
+</html>
